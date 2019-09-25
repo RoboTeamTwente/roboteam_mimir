8b72db3c8378ab196f1a7f48c010a93a50b3e16c
rolfvdhulst <rolfvdhulst@gmail.com>
2019-09-25 14:49:04 +0200

    some resemblance of 3d drawing

--- src/interface/DebugVisualization.cpp
+++ src/interface/DebugVisualization.cpp
@@ -20,6 +20,7 @@ namespace interface {
     // This function initializes everything GL. Is only called once to startUp GL contexts
     void DebugVisualization::initializeGL() {
         QOpenGLFunctions *f=QOpenGLContext::currentContext()->functions();
+
         //initialize openGL resources
         bool vboCreate=vbo.create();
         bool vaoCreate=vao.create();
@@ -37,12 +38,23 @@ namespace interface {
         //create a black background
         f->glClearColor(0.0,0.0,0.0,1.0);
         float vertices[] = {
-                -0.5f, -0.5f, 0.0f,
-                0.5f, -0.5f, 0.0f,
-                0.0f,  0.5f, 0.0f
+                -0.5f, -0.5f, 0.0f,1.0f,
+                0.5f, -0.5f, 0.0f, 1.0f,
+                0.5f, -0.5f, 0.0f, 1.0f,
+                0.0f,  0.5f, 0.0f, 1.0f,
+                0.0f,  0.5f, 0.0f, 1.0f,
+                -0.5f, -0.5f, 0.0f,1.0f,
+                0.0f, 0.0f, -0.5f, 1.0f,
+                -0.5f, -0.5f, 0.0f,1.0f,
+                0.0f, 0.0f, -0.5f, 1.0f,
+                0.5f, -0.5f, 0.0f, 1.0f,
+                0.0f, 0.0f, -0.5f, 1.0f,
+                0.0f,  0.5f, 0.0f, 1.0f
         };
+        f->glEnable(GL_DEPTH_TEST);//enable depth buffer
+        f->glEnable(GL_CULL_FACE); //enable backface culling
         vbo.bind();
-        vbo.allocate(vertices,9*sizeof(float));
+        vbo.allocate(vertices,12*4*sizeof(float));
     }
     // Called whenever window size is changed to detail how visualization should change.
     void DebugVisualization::resizeGL(int w, int h) {
@@ -52,14 +64,24 @@ namespace interface {
     void DebugVisualization::paintGL() {
         //clear buffers so that throw out old visualizations
         QOpenGLFunctions *f=QOpenGLContext::currentContext()->functions();
+        f->glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the buffers from previous iteration
+        //set up the view
+        QMatrix4x4 projection;
+        projection.perspective(45.0,4.0/3.0,0.1,100.0);
+        QMatrix4x4 view;
 
-
+        view.lookAt({4.0,4.0,3.0},{0.0,0.0,0.0},{0.0,1.0,0.0});
+        QMatrix4x4 model;
+        model.setToIdentity();
+        QMatrix4x4 mvp=projection*view*model;
+        shaderProgram.setUniformValue("mvp_matrix",mvp);
         vbo.bind();
         quintptr offset = 0;
         int vertexLocation= shaderProgram.attributeLocation("aPos");
         shaderProgram.enableAttributeArray(vertexLocation);
-        shaderProgram.setAttributeBuffer(vertexLocation,GL_FLOAT, offset, 3);
-        f->glDrawArrays(GL_TRIANGLES, 0, 3);
+        shaderProgram.setAttributeBuffer(vertexLocation,GL_FLOAT, offset, 4);
+        f->glDrawArrays(GL_LINES, 0, 12);
+
 
     }
 
--- src/interface/vshader.glsl
+++ src/interface/vshader.glsl
@@ -1,7 +1,7 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;
-
+layout (location = 0) in vec4 aPos;
+uniform mat4 mvp_matrix;
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    gl_Position = mvp_matrix * aPos;
 }

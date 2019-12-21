6e01f217db511828192fe0873cc147774f613403
rolfvdhulst <rolfvdhulst@gmail.com>
2019-12-21 12:06:28 +0100

    Add ball randomness

--- src/simulator/utilities/Random.cpp
+++ src/simulator/utilities/Random.cpp
@@ -3,19 +3,25 @@
 //
 
 #include "Random.h"
-Random::Random(double xRange, double yRange, double orRange, int xSeed, int ySeed, int orientationSeed,
-        int vanishingSeed) {
+Random::Random(double xRange, double yRange, double orRange, double ballxRange, double ballYRange, int xSeed, int ySeed,
+        int orientationSeed,
+        int vanishingSeed, int ballXSeed, int ballYSeed) {
 
-    xGenerator=std::mt19937(xSeed);
-    yGenerator=std::mt19937(ySeed);
-    orientationGenerator=std::mt19937(orientationSeed);
+    xGenerator = std::mt19937(xSeed);
+    yGenerator = std::mt19937(ySeed);
+    orientationGenerator = std::mt19937(orientationSeed);
 
-    xDist=std::normal_distribution<double>(0,xRange);
-    yDist=std::normal_distribution<double>(0,yRange);
-    orDist=std::normal_distribution<double>(0,orRange);
+    xDist = std::normal_distribution<double>(0, xRange);
+    yDist = std::normal_distribution<double>(0, yRange);
+    orDist = std::normal_distribution<double>(0, orRange);
 
-    vanishGenerator=std::mt19937(vanishingSeed);
-    vanishDist=std::uniform_real_distribution<double>(0,1);
+    vanishGenerator = std::mt19937(vanishingSeed);
+    vanishDist = std::uniform_real_distribution<double>(0, 1);
+
+    ballXGenerator = std::mt19937(ballXSeed);
+    ballYGenerator = std::mt19937(ballYSeed);
+    ballXDist = std::normal_distribution<double>(0, ballxRange);
+    ballYDist = std::normal_distribution<double>(0, ballYRange);
 }
 
 double Random::getOrientation() {
@@ -30,12 +36,24 @@ double Random::getY() {
 double Random::getVanishing() {
     return vanishDist(vanishGenerator);
 }
+double Random::getBallX() {
+    return ballXDist(ballXGenerator);
+}
+double Random::getBallY() {
+    return ballYDist(ballYGenerator);
+}
 void Random::setOrientationRange(double orientationRange) {
-    orDist=std::normal_distribution<double>(0,orientationRange);
+    orDist = std::normal_distribution<double>(0, orientationRange);
 }
 void Random::setXRange(double xRange) {
-    xDist=std::normal_distribution<double>(0,xRange);
+    xDist = std::normal_distribution<double>(0, xRange);
 }
 void Random::setYRange(double yRange) {
-    yDist=std::normal_distribution<double>(0,yRange);
+    yDist = std::normal_distribution<double>(0, yRange);
+}
+void Random::setBallXRange(double xRange) {
+    ballXDist = std::normal_distribution<double>(0, xRange);
+}
+void Random::setBallYRange(double yRange) {
+    ballYDist = std::normal_distribution<double>(0, yRange);
 }
--- src/simulator/utilities/Random.h
+++ src/simulator/utilities/Random.h
@@ -8,24 +8,34 @@
 
 class Random {
     public:
-        Random(double xRange, double yRange, double orRange, int xSeed=42, int ySeed=1337, int orientationSeed=2193, int vanishingSeed=1578);
+        Random(double xRange, double yRange, double orRange, double ballXRange, double ballYRange, int xSeed = 42,
+                int ySeed = 1337, int orientationSeed = 2193, int vanishingSeed = 1578, int ballXSeed = 83,
+                int ballYSeed = 92);
         void setXRange(double xRange);
         void setYRange(double yRange);
         void setOrientationRange(double orientationRange);
+        void setBallXRange(double xRange);
+        void setBallYRange(double yRange);
 
         double getX();
         double getY();
         double getOrientation();
         double getVanishing();
+        double getBallY();
+        double getBallX();
     private:
         std::mt19937 vanishGenerator;
         std::mt19937 orientationGenerator;
         std::mt19937 xGenerator;
         std::mt19937 yGenerator;
+        std::mt19937 ballXGenerator;
+        std::mt19937 ballYGenerator;
 
         std::normal_distribution<double> xDist;
         std::normal_distribution<double> yDist;
         std::normal_distribution<double> orDist;
+        std::normal_distribution<double> ballXDist;
+        std::normal_distribution<double> ballYDist;
 
         std::uniform_real_distribution<double> vanishDist;
 };
